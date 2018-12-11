06b1aab531524024cd623566e9b88c9db90f958f
Andrea <26384252+kaywinnet@users.noreply.github.com>
2018-12-11 15:07:05 +0100

    Update Get_GENE_from_IDq.R
    Now returns the original gene if no gene name is found in the redundancy or if the gene found in the redundancy is of type LOC*.
    Now removes rows where no gene name is found in any field.
    Now specifies procedence (species) of the extracted gene name.

--- Get_GENE_from_IDq.R
+++ Get_GENE_from_IDq.R
@@ -1,70 +1,105 @@
 #######################################
 # Created on Wed Dec  5 11:22:15 2018 #
 #       author: Andrea Laguillo       #
+#           CNIC Proteomics           #
 #######################################
 
 #install.packages("data.table")
+#install.packages("rlist")
+#install.packages("tidyr")
 library(data.table)
+library(rlist)
+library(tidyr)
 
 # PARAMETERS
-filepath <- ("input")           # Set path to ID-q file
-output <- ("output")            # Set path to output file
-get_pig_redundancies <- 1       # 0 for NO PIG only GET GENE from FASTAProteinDescription, 1 for YES-PIG and GET GENE
+filepath <- ("input")               # Set path to ID-q file
+output <- ("output")                # Set path to output file
+get_species_redundancies <- 1       # 0 for NO redundancies only GET GENE, 1 for YES redundancies and GET GENE
+preference_species <- "Sus scrofa"  # Set species (as it appears on ID-q file) to check for redundancies,
+                                    # ignored if get_species_redundancies set to 0
 
 ###################################################################################################
 
 # Get data from FASTA file
-IDq <- fread(file = filepath, sep="\t", header=TRUE)
+IDq <- fread(file=filepath, sep="\t", header=TRUE)
 
 # Define function to get Gene name
-getGeneName <- function(x) {
-               if (is.na(x["Preference_Pig"])) {
+getGeneName <- function(x, preference_species=NA) {
+               if (is.na(x["Preference_Species"])) { # Not looking at redundancies
                  gene <- substring(regmatches(x["FASTAProteinDescription"],
                                               regexpr("GN=[a-zA-Z0-9]*",
                                                       x["FASTAProteinDescription"])), 4)
                  if (length(gene) == 0) {
-                   gene <- "Not Found"
+                   gene <- NA
                  }
                  gene <- toString(gene)
                  return(gene)
                }
                else {
-                 if (x["Preference_Pig"] != "No") {
-                 gene <- substring(regmatches(x["Preference_Pig"],
+                 if (x["Preference_Species"] != "No") { # Redundacy found for that species
+                   gene <- substring(regmatches(x["Preference_Species"],
                                               regexpr("GN=[a-zA-Z0-9]*",
-                                                      x["Preference_Pig"])), 4)
-                 if (length(gene) == 0) {
-                   gene <- "Not Found"
-                 }
-                 gene <- toString(gene)
-                 return(gene)
-                 }
-                 else {
+                                                      x["Preference_Species"])), 4)
+                   procedence <- preference_species
+                   gene <- paste(gene, procedence, sep=",")
+                   if (length(gene) == 0) { # Gene not found
+                     # Get from original column instead
+                     gene <- substring(regmatches(x["FASTAProteinDescription"],
+                                                regexpr("GN=[a-zA-Z0-9]*",
+                                                        x["FASTAProteinDescription"])), 4)
+                     if (length(gene) == 0) {
+                       gene <- NA
+                       return(gene)
+                     }
+                     procedence <- "Homo sapiens"
+                     gene <- paste(gene, procedence, sep=",")
+                   }
+                   if (substring(gene, 1, 3) == "LOC") { # Gene is LOC*
+                     # Get from original column instead
+                     gene <- substring(regmatches(x["FASTAProteinDescription"],
+                                                  regexpr("GN=[a-zA-Z0-9]*",
+                                                          x["FASTAProteinDescription"])), 4)
+                     if (length(gene) == 0) {
+                       gene <- NA
+                       return(gene)
+                     }
+                     procedence <- "Homo sapiens"
+                     gene <- paste(gene, procedence, sep=",")
+                   }
+                   #gene <- toString(gene)
+                   return(gene)
+                   }
+                 else { # Redundacy not found for that species
                    gene <- substring(regmatches(x["FASTAProteinDescription"],
                                                 regexpr("GN=[a-zA-Z0-9]*",
                                                         x["FASTAProteinDescription"])), 4)
                    if (length(gene) == 0) {
-                     gene <- "Not Found"
+                     gene <- NA
+                     return(gene)
                    }
-                   gene <- toString(gene)
+                   #gene <- toString(gene)
+                   procedence <- substring(regmatches(x["FASTAProteinDescription"],
+                                                      regexpr("OS=[a-zA-Z0-9]* [a-zA-Z0-9]*",
+                                                              x["FASTAProteinDescription"])), 4)
+                   gene <- paste(gene, procedence, sep=",")
                    return(gene)
                  }
                }
                } 
 
-# Define function to find Pig redundancies
-getPigRedundancies <- function(x) {
+# Define function to find Species redundancies
+getSpeciesRedundancies <- function(x, preference_species) {
                       header <- x["FASTAProteinDescription"]
                       h_species <- substring(regmatches(x["FASTAProteinDescription"],
-                                                        regexpr("OS=[a-zA-Z0-9]*",
+                                                        regexpr("OS=[a-zA-Z0-9]* [a-zA-Z0-9]*",
                                                                 x["FASTAProteinDescription"])), 4)
-                      if (h_species == "Homo"){
+                      if (h_species == "Homo sapiens"){
                         redundancies <- x["Redundances"]
                         redundancies <- strsplit(redundancies, " -- ")
                         for (i in redundancies[[1]]){
-                          r_species <- substring(regmatches(i, regexpr("OS=[a-zA-Z0-9]*",
+                          r_species <- substring(regmatches(i, regexpr("OS=[a-zA-Z0-9]* [a-zA-Z0-9]*",
                                                                     i)), 4)
-                          if (r_species == "Sus"){
+                          if (r_species == preference_species){
                             return(as.character(i))
                             break
                           }
@@ -74,18 +109,27 @@ getPigRedundancies <- function(x) {
                       }
 
 # Create new columns
-IDq[, c("Preference_Pig", "Gene")] <- NA
-IDq$Preference_Pig <- as.character(IDq$Preference_Pig)
+IDq[, c("Preference_Species", "Gene")] <- NA
+IDq$Preference_Species <- as.character(IDq$Preference_Species)
 IDq$Gene <- as.character(IDq$Gene)
 
-if (get_pig_redundancies == 0) {
+if (get_species_redundancies == 0) {
   IDq$Gene <- apply(IDq, 1, getGeneName) 
-  IDq$Preference_Pig <- NULL
+  IDq$Preference_Species <- NULL
 }
-if (get_pig_redundancies == 1) {
-  IDq$Preference_Pig <- apply(IDq, 1, getPigRedundancies)
-  IDq$Gene <- apply(IDq, 1, getGeneName)
+if (get_species_redundancies == 1) {
+  IDq$Preference_Species <- apply(IDq, 1, getSpeciesRedundancies, preference_species)
+  IDq$Gene <- apply(IDq, 1, getGeneName, preference_species)
+  #TODO: separate last column
+  IDq <- IDq%>% separate(Gene, c("Gene", "Procedence"), sep=",")
 }
+
+# Remove rows for which there is no Gene name
+IDq <- IDq[complete.cases(IDq[,"Gene"]),]
+
+column_name <- gsub(" ", "_", preference_species)
+column_name <- paste("Redundancy_", column_name, sep="")
+names(IDq)[names(IDq) == "Preference_Species"] <- column_name
 
 # Write to file
 fwrite(IDq, output, sep="\t")
